5aa54d01a5964d9143903cd0778fa1662a9f920e
wesley-caetano <130664384+wesley-caetano@users.noreply.github.com>
2024-03-13 21:48:50 -0300

    Update main.tf
    Load balancer sendo provisionado

--- main.tf
+++ main.tf
@@ -2,30 +2,30 @@
 
 terraform {
   required_version = "1.7.4" # versão do terraform
-  backend "local" {    # local para salvar o arquivo
-    path = "terraform.tfstate"  
+  backend "local" {          # local para salvar o arquivo
+    path = "terraform.tfstate"
   }
 }
 
 
-resource "aws_vpc" "main-vpc" {  # recurso e nome do recurso no arquivo terraform
-  cidr_block = "10.20.0.0/16"
-   tags = {
+resource "aws_vpc" "main-vpc" { # recurso e nome do recurso no arquivo terraform
+  cidr_block = var.vpc_cidr_block
+  tags = {
     Name = "vpc-desafio"
   }
 }
 
 resource "aws_internet_gateway" "gate-net" {
-  vpc_id = aws_vpc.main-vpc.id   # para vpc id, recurso.nome no tf.id
+  vpc_id = aws_vpc.main-vpc.id # para vpc id, recurso.nome no tf.id
 
   tags = {
-    Name = "gaye-net-desafio"
+    Name = "gate-net-desafio"
   }
 }
 resource "aws_subnet" "sub_net_pa" {
-  vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = "10.20.1.0/24" # lembrar de colocar aspas em string
-  availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = var.public_subnet_1_cidr
+  availability_zone = var.public_subnet_1_az
 
   tags = {
     Name = "Pub-a"
@@ -33,29 +33,29 @@ resource "aws_subnet" "sub_net_pa" {
   map_public_ip_on_launch = true
 }
 resource "aws_subnet" "sub_net_pb" {
-  vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = "10.20.2.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = var.public_subnet_2_cidr
+  availability_zone = var.public_subnet_2_az
 
   tags = {
     Name = "Pub-B"
   }
-  map_public_ip_on_launch = true  # habiltando ip publico ao inicializar a sub net
+  map_public_ip_on_launch = true # habiltando ip publico ao inicializar a sub net
 }
 
 resource "aws_subnet" "subs-pva" {
-  vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = "10.20.3.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = var.private_subnet_1_cidr
+  availability_zone = var.private_subnet_1_az
 
   tags = {
     Name = "Priv-a"
   }
 }
 resource "aws_subnet" "sub-pvb" {
-  vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = "10.20.4.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id            = aws_vpc.main-vpc.id
+  cidr_block        = var.private_subnet_2_cidr
+  availability_zone = var.private_subnet_2_az
 
   tags = {
     Name = "Priv-B"
@@ -72,7 +72,7 @@ resource "aws_route_table" "rota_public" {
 resource "aws_route" "rota_gate_net" {
   route_table_id         = aws_route_table.rota_public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.gate-net.id
+  gateway_id             = aws_internet_gateway.gate-net.id
 
 }
 
@@ -89,7 +89,7 @@ resource "aws_route_table_association" "public_subnet_2" {
 resource "aws_eip" "ip_elastico" {
   tags = {
     Name = "Ip_elastico_1"
-    
+
   }
 }
 
@@ -97,7 +97,7 @@ resource "aws_eip" "ip_elastico" {
 
 resource "aws_nat_gateway" "nat_gate_1" {
   allocation_id = aws_eip.ip_elastico.id
-  subnet_id = aws_subnet.sub_net_pa.id
+  subnet_id     = aws_subnet.sub_net_pa.id
 
   tags = {
     Name = "NatGat1"
@@ -115,7 +115,7 @@ resource "aws_route_table" "rota_privada" {
 resource "aws_route" "rota_nat_gate_net" {
   route_table_id         = aws_route_table.rota_privada.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat_gate_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gate_1.id
 
 }
 
@@ -133,37 +133,37 @@ resource "aws_route_table_association" "privada_subnet_2" {
 resource "aws_security_group" "my_security_group" {
   name        = "Meu-grupo-de-seguranca"
   description = "My Security Group for SSH, HTTP, and HTTPS"
-  vpc_id = aws_vpc.main-vpc.id
+  vpc_id      = aws_vpc.main-vpc.id
 
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["186.249.155.155/32"]  # Substitua pelo bloco CIDR da sua rede para SSH
+    cidr_blocks = ["186.249.155.155/32"] # Substitua pelo bloco CIDR da sua rede para SSH
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite HTTP de qualquer lugar
+    cidr_blocks = ["0.0.0.0/0"] # Permite HTTP de qualquer lugar
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Permite HTTPS de qualquer lugar
+    cidr_blocks = ["0.0.0.0/0"] # Permite HTTPS de qualquer lugar
   }
 }
 
 resource "aws_instance" "teste_ec2" {
-  ami           = "ami-0cd59ecaf368e5ccf"  # Substitua pelo ID da AMI do Amazon Linux 2
-  instance_type = "t2.micro"                # Tipo de instância desejado
-  subnet_id = aws_subnet.sub_net_pa.id
+  ami           = var.ec2_ami_id
+  instance_type = var.instance_type              # Tipo de instância desejado
+  subnet_id     = aws_subnet.sub_net_pa.id
 
-  key_name      = "CHAVE-PROJETO"    # Substitua pelo nome da sua chave existente
+  key_name               = "CHAVE-PROJETO" # Substitua pelo nome da sua chave existente
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
 
   tags = {
@@ -174,7 +174,7 @@ resource "aws_instance" "teste_ec2" {
 resource "aws_security_group" "my_db_gp_seguranca" {
   name        = "mydbsecuritygroup"
   description = "My DB Security Group"
-  vpc_id = aws_vpc.main-vpc.id
+  vpc_id      = aws_vpc.main-vpc.id
 
   ingress {
     from_port   = 3306
@@ -191,11 +191,11 @@ resource "aws_db_instance" "meu_db" {
   engine                 = "mysql"
   engine_version         = "5.7.44"
   instance_class         = "db.t2.micro"
-  username               = "testedb"
-  password               = "teste123"
+  username               = "rigel"
+  password               = "rigel123"
   publicly_accessible    = false
-  skip_final_snapshot = true
-  multi_az = true
+  skip_final_snapshot    = true
+  multi_az               = true
   db_subnet_group_name   = aws_db_subnet_group.meu_gp_subnet.id
   vpc_security_group_ids = [aws_security_group.my_db_gp_seguranca.id]
 
@@ -205,3 +205,39 @@ resource "aws_db_subnet_group" "meu_gp_subnet" {
   name       = "my-db-subnet-group"
   subnet_ids = [aws_subnet.subs-pva.id, aws_subnet.sub-pvb.id]
 }
+
+resource "aws_lb" "my_load_balancer" {
+  name               = "my-public-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  
+  subnets = [aws_subnet.sub_net_pa.id, aws_subnet.sub_net_pb.id]
+  
+  tags = {
+    Name = "my-public-load-balancer"
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.my_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "my_target_group" {
+  name        = "my-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main-vpc.id
+  target_type = "instance"
+}
+resource "aws_lb_target_group_attachment" "my_target_group_attachment" {
+  target_group_arn = aws_lb_target_group.my_target_group.arn
+  target_id        = aws_instance.teste_ec2.id  # Substitua pelo ID da sua instância
+}
+
