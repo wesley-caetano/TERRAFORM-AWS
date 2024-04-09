92dbb3ee8f0cf164a12dae789657fa128893823d
wesley-caetano <130664384+wesley-caetano@users.noreply.github.com>
2024-04-08 22:38:46 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,57 +2,42 @@ variable "vpc_cidr_block" {
   default = "10.20.0.0/16"
 }
 
-variable "public_subnet_1_cidr" {
+variable "subnet_publica_a_cidr" {
   default = "10.20.1.0/24"
 }
 
-variable "public_subnet_2_cidr" {
+variable "subnet_publica_b_cidr" {
   default = "10.20.2.0/24"
 }
 
-variable "private_subnet_1_cidr" {
+variable "subnet_privada_a_cidr" {
   default = "10.20.3.0/24"
 }
 
-variable "private_subnet_2_cidr" {
+variable "subnet_privada_b_cidr" {
   default = "10.20.4.0/24"
 }
 
-variable "public_subnet_1_az" {
-  type    = string
+variable "az_a" {
   default = "us-east-1a"
 }
 
-variable "public_subnet_2_az" {
-  type    = string
+variable "az_b" {
   default = "us-east-1b"
-}
-
-variable "private_subnet_1_az" {
-  type    = string
-  default = "us-east-1a"
-}
-
-variable "private_subnet_2_az" {
-  type    = string
-  default = "us-east-1b"
-}
-
-variable "ec2_ami_id" {
-  type    = string
-  default = "ami-0cd59ecaf368e5ccf" # Insira aqui o ID da AMI desejada
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "key_name" {
-  default = "SUA-CHAVE"
 }
 
 variable "meu_ip_cidr" {
-  description = "Bloco CIDR endereço IP na AWS"
-  type        = string
-  default     = "xxx.xxx.xxx/xx"                  # seu ip
+  default = "XXX.XXX.XXX.XXX.XX"
+}
+
+variable "key-name" {
+  default = "SUA-CHAVE"
+}
+
+variable "tipo-instancia" {
+  default = "t2.micro"
+}
+
+variable "ec2_ami_id" {
+  default = "ami-0cd59ecaf368e5ccf"
 }
